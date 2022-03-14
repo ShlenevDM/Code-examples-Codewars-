097db89981ebb7315ecaebf67d6681b883057f97
ShlenevDM <93936631+ShlenevDM@users.noreply.github.com>
2022-03-14 09:09:25 +0300

    init Linear Equation Solver

---
+++ Linear Equation Solver.py
@@ -0,0 +1,29 @@
+import re
+
+equations = ("2x-y+3x-1=-2y+3x+9y+9", "-y+8+x+2y=15")
+
+# find all variables
+variables = set()
+for eq in equations:
+    variables = variables | set(re.findall(r'[a-zA-Z]+', eq))
+#print(variables)
+
+# verify number of equations and variables
+n = len(variables)
+m = len(equations)
+
+# initilize matrices
+A = [[0]*n]*n
+B = [0]*n
+"""print(A)
+print(B)"""
+
+def sum_coef(regex, eq):
+    return sum(map(float, re.findall(regex, eq)))
+
+# fill matrices with coefficients
+for i in range(n):
+    left, right = re.split(r'[=]', equations[i])
+    B[i] = sum_coef(r'[+-]?\d+\b', right) - sum_coef(r'[+-]?\d+\b', left)
+
+print(B)
