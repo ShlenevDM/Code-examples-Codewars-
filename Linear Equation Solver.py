eb0196acb1a731317130822e10e054fc5c9f2117
ShlenevDM <93936631+ShlenevDM@users.noreply.github.com>
2022-03-16 21:46:21 +0300

    Linear Equation Solver.py - удаление дублирующихся уравнений

--- Linear Equation Solver.py
+++ Linear Equation Solver.py
@@ -1,6 +1,6 @@
 import re
 
-equations = ("2x-y+3x=-2y+3x+9y", "-y+x+2y=5")
+equations = ("2x-y+3x=-2y+3x+9y", "-y+x+2y=5", "-2y+2x+4y=10")
 
 # find all variables
 variables = set()
@@ -8,18 +8,17 @@ for eq in equations:
     variables = variables | set(re.findall(r'[a-zA-Z]+', eq))
 variables = sorted(list(variables))
 
-# verify number of equations and variables
 n = len(variables)
 m = len(equations)
 
 # initialize matrices
-A = [[0 for row_number in range(n)] for column_number in range(n)]
-B = [0] * n
+A = [[0 for column_number in range(n)] for row in range(m)]
+B = [0] * m
 
 
 # parsing of the equation
 def matrix_row(eqs):
-    for row_number in range(n):
+    for row_number in range(m):
         parsed = re.split('([+=-])', eqs[row_number])
         c = 1
         for i in range(len(parsed)):
@@ -40,7 +39,34 @@ def matrix_row(eqs):
                     A[row_number][column_number] += c * int(a)
 
 
-matrix_row(equations)  # not to forget to call this function
+matrix_row(equations)  # fill the matrix with coefficients
+
+A_B = [A[i] + [B[i]] for i in range(m)]
+#print(A_B)
+for i in range(m):
+    for j in range(n + 1):
+        if A_B[i][j] == 0:
+            continue
+        else:
+            a = A_B[i][j]
+            break
+    for j in range(n + 1):
+        A_B[i][j] /= a
+#print(A_B)
+
+i = 0
+while i < m:
+    if A_B.count(A_B[i]) > 1:
+        A.remove(A[i])
+        B.remove(B[i])
+        A_B.remove(A_B[i])
+        m -= 1
+    else:
+        i += 1
+
+print(A)
+print(B)
+
 
 # minor
 def minor(m, i, j):
@@ -54,5 +80,4 @@ def det(matrix):
     elif len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     else:
-        return sum(matrix[0][i] *(-1) ** i * det(minor(matrix, 0, i)) for i in range(len(matrix)))
-
+        return sum(matrix[0][i] * (-1) ** i * det(minor(matrix, 0, i)) for i in range(len(matrix)))
