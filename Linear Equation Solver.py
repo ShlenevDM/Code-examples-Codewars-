e95614fbfe230570f24f33da05c2423708911332
ShlenevDM <93936631+ShlenevDM@users.noreply.github.com>
2022-03-16 15:35:32 +0300

    Linear Equation Solver.py - парсер для уравнений готов и отлажен

--- Linear Equation Solver.py
+++ Linear Equation Solver.py
@@ -1,50 +1,49 @@
 import re
 
-equations = ("x-y+3x-1=-2y+3x+9y+9", "-3y+8+2x+2y=15")
+equations = ("2x-y+3x=-2y+3x+9y", "-y+x+2y=5")
 
 # find all variables
 variables = set()
 for eq in equations:
     variables = variables | set(re.findall(r'[a-zA-Z]+', eq))
 variables = sorted(list(variables))
-#print(variables)
+# print(variables)
 
 # verify number of equations and variables
 n = len(variables)
 m = len(equations)
 
-# initilize matrices
-A = [[0]*n]*n
-B = [0]*n
-"""print(A)
-print(B)"""
+# initialize matrices
+A = [[0 for row_number in range(n)] for column_number in range(n)]
+B = [0] * n
 
-# parcing of the equation
-"""def matrix_row(eqs):
+
+# parsing of the equation
+def matrix_row(eqs):
     for row_number in range(n):
         parsed = re.split('([+=-])', equations[row_number])
+        c = 1
+        for i in range(len(parsed)):
+            if parsed[i].isdigit() and parsed[i - 1] == '-':
+                B[row_number] -= c * int(parsed[i])
+            elif parsed[i].isdigit():
+                B[row_number] += c * int(parsed[i])
         for column_number in range(n):
             var = variables[column_number]
-            c = 1   # positive before =, negative after =
+            c = 1  # positive before =, negative after =
             for i in range(len(parsed)):
                 if parsed[i] == '=': c = -1
-                if var in parsed[i] and eq[i - 1] == '-':
-                    A[row_number][column_number] -= c * int(eq[i].strip(var))
+                if var in parsed[i] and parsed[i - 1] == '-':
+                    a = parsed[i].strip(var) or 1
+                    A[row_number][column_number] -= c * int(a)
                 elif var in parsed[i]:
-                    A[row_number][column_number] += c * int(eq[i].strip(var))"""
-def matrix_row(eqs):
-    parsed = []
-    for row_number in range(len(eqs)):
-        parsed.append(re.split('([+=-])', eqs[row_number]))
-    return parsed
+                    a = parsed[i].strip(var) or 1
+                    A[row_number][column_number] += c * int(a)
 
 
-print(matrix_row(equations))
-#print(A)
-
-
-
-
+matrix_row(equations)
+print(A)
+print(B)
 """def sum_coef(regex, eq):
     return sum(map(int, re.findall(regex, eq)))
 
