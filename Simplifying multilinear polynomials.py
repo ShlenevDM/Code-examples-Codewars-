cd873e77e02a2270e85cde8b13a096e2fe900cdb
ShlenevDM <93936631+ShlenevDM@users.noreply.github.com>
2022-03-19 22:05:21 +0300

    Simplifying multilinear polynomials.py - парсер для уравнений готов

---
+++ Simplifying multilinear polynomials.py
@@ -0,0 +1,25 @@
+import re
+
+poly = "-a+52ab+3a-c-2a-3ba"
+
+var = list(set([''.join(sorted(el)) for el in re.findall(r'[a-z]+', poly)]))
+#print(var)
+
+parsed = re.split('([+=-])', poly)
+#print(parsed)
+
+coefficients = {v:0 for v in var}
+#print(coefficients)
+
+for i in range(len(var)):
+    for j in range(len(parsed)):
+        #a = parsed[j].strip(var[i]) or 1
+        if ''.join(sorted(parsed[j].lstrip('0123456789'))) == var[i] and parsed[j - 1] == '-':
+            a = parsed[j].strip(var[i]) or 1
+            coefficients[var[i]] -= int(a)
+        elif ''.join(sorted(parsed[j].lstrip('0123456789'))) == var[i]:
+            a = parsed[j].strip(var[i]) or 1
+            coefficients[var[i]] += int(a)
+
+print(coefficients)
+
